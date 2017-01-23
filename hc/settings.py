2c0c71ed278cd3bd8ea3814c66ef235b47fbd63c
karendi <sharonkarendi16@gmail.com>
2017-01-23 14:29:44 +0300

    [chore #137856587]deleted the commented out old database settings

--- hc/settings.py
+++ hc/settings.py
@@ -92,35 +92,6 @@ ALLOWED_HOSTS = ['*']
 
 DEBUG = True
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME':   './hc.sqlite',
-#     }
-# }
-#
-# # You can switch database engine to postgres or mysql using environment
-# # variable 'DB'. Travis CI does this.
-# if os.environ.get("DB") == "postgres":
-#     DATABASES = {
-#         'default': {
-#             'ENGINE':   'django.db.backends.postgresql',
-#             'NAME':     'hc',
-#             'USER':     'postgres',
-#             'TEST': {'CHARSET': 'UTF8'}
-#         }
-#     }
-#
-# if os.environ.get("DB") == "mysql":
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.mysql',
-#             'USER':     'root',
-#             'NAME':     'hc',
-#             'TEST': {'CHARSET': 'UTF8'}
-#         }
-#     }
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
