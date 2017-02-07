e30153c2afee3e65c8acf4a87b7125012988cee5
sirjmkitavi <kitavimjoseph@gmail.com>
2017-02-07 14:35:01 +0300

    Renamed variable names

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -7,8 +7,10 @@ class AddCheckTestCase(BaseTestCase):
     def test_it_works(self):
         url = "/checks/add/"
         self.client.login(username="alice@example.org", password="password")
-        r = self.client.post(url)
-        self.assertRedirects(r, "/checks/")
+        response = self.client.post(url)
+        self.assertRedirects(response, "/checks/")
         assert Check.objects.count() == 1
 
-    ### Test that team access works
+    def test_team_access_works(self):
+        """ Test that team access works."""
+
