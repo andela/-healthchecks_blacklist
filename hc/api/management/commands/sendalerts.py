a6f83663fd433da9699d071927f01991d1f213e2
karendi <sharonkarendi16@gmail.com>
2017-02-22 07:55:19 +0300

    fixed send alert bug

--- hc/api/management/commands/sendalerts.py
+++ hc/api/management/commands/sendalerts.py
@@ -11,19 +11,24 @@ executor = ThreadPoolExecutor(max_workers=10)
 logger = logging.getLogger(__name__)
 
 
+
 class Command(BaseCommand):
     help = 'Sends UP/DOWN email alerts'
 
     def handle_many(self):
         """ Send alerts for many checks simultaneously. """
+        now = timezone.now()
         query = Check.objects.filter(user__isnull=False).select_related("user")
 
-        now = timezone.now()
+        nags = query.filter(status="down", nag_after__lt=now)
+
         going_down = query.filter(alert_after__lt=now, status="up")
-        going_up = query.filter(alert_after__gt=now, status="down")
-        nag = query.filter(status="down", nag__isnull=False, nag_after__lt=now)
+        checks = list(going_down.iterator()) + list(nags.iterator())
+        if not nags:
+            going_up = query.filter(alert_after__gt=now, status="down")
+            checks += list(going_up.iterator()) 
         # Don't combine this in one query so Postgres can query using index:
-        checks = list(going_down.iterator()) + list(going_up.iterator()) + list(nag.iterator())
+        
         if not checks:
             return False
 
@@ -43,19 +48,23 @@ class Command(BaseCommand):
 
         # Save the new status. If sendalerts crashes,
         # it won't process this check again.
-        if check.nag_after:
-            check.update_nag_after()
-        check.status = check.get_status()
-        check.save()
 
+        now = timezone.now()
         tmpl = "\nSending alert, status=%s, code=%s\n"
         self.stdout.write(tmpl % (check.status, check.code))
+        self.stdout.write("alert_after: {} nag_after: {} nag: {} last_nag_alert: {}".format(check.alert_after, check.nag_after, check.nag, check.last_nag_alert))
         errors = check.send_alert()
         for ch, error in errors:
             self.stdout.write("ERROR: %s %s %s\n" % (ch.kind, ch.value, error))
 
+        if check.status == "down":
+            check.update_nag()
+        check.status = check.get_status()
+        check.save()
+
         connection.close()
         return True
+
 
     def handle(self, *args, **options):
         self.stdout.write("sendalerts is now running")
